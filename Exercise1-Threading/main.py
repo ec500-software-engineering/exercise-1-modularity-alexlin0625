56fac896c97421549db3ecbd82707c3d46abe41a
Alex Jeffrey Lin <43394154+alexlin0625@users.noreply.github.com>
2019-02-14 22:17:40 -0500

    Update main.py

--- Exercise1-Threading/main.py
+++ Exercise1-Threading/main.py
@@ -6,27 +6,26 @@ import alert_system
 import output
 
 
-def run_threads():
-    while True:
-        db = connect_db()
-        PatientInfo = getPatientInfo()
-        SensorData = readSensorData(PatientInfo)
-
-        # create_table(db) #create table only the first target
-        insert_db(db, PatientInfo, SensorData)
-        # search_db(db)
-        # delete_db(db) #only use it if you want to delete table data for the patient
-        # update_db(db)
-        sleep(2)
-        alert_json = alert_system.alertCheck(SensorData)
-        patient1 = output.patient()
-        patient1.recieveFromAlert(alert_json)
-        patient1.send_alert_to_UI()
-        sleep(2)
+def run_threads(PatientInfo, SensorData):
+    db = connect_db()
+    # create_table(db) # create table only the first time
+    insert_db(db, PatientInfo, SensorData)
+    # search_db(db) # search for patient's current information
+    # delete_db(db) # only use it if you want to delete table data for the patient
+    # update_db(db) # update elements in patient's current information
+    alert_json = alert_system.alertCheck(SensorData)
+    patient1 = output.patient()
+    patient1.recieveFromAlert(alert_json)
+    patient1.send_alert_to_UI()
 
 
-t1 = Thread(target=run_threads())
-t1.start()
+while True:
+    PatientInfo = getPatientInfo()
+    SensorData = readSensorData(PatientInfo)
+    t1 = Thread(target=run_threads, args=(PatientInfo, SensorData))
+    t1.start()
+    sleep(3)
+
 
 
 
